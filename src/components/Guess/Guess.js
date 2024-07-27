690c5202dcc9f20ef2299236db5ff6e85465557f
aydamacink <dayanamick@gmail.com>
2024-07-27 23:37:43 +0200

    challenge 4

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers.js'; // returns an object with letter and status
 
-function Guess({ value }) {
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}` : 'cell';
+  return <span className={className}>{letter}</span>;
+}
+
+function Guess({ value, answer }) {
+  result = checkGuess(value, answer);
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key={num} className="cell">
-          {value ? value[num] : undefined}
-        </span>
+        <Cell
+          key={num}
+          status={result ? result[num].status : undefined}
+          letter={result ? result[num].letter : undefined}
+        />
       ))}
     </p>
   );
